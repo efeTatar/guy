f649fa1cfaca398932d6e7638dfb2916e882fae8
NathanSignoud <signoudnat@cy-tech.fr>
2022-05-19 16:02:36 +0200

    idenqual

--- compression.c
+++ compression.c
@@ -1,1 +1,8 @@
 #include "ppm_lib.h"
+
+void compressionManager(pixel_structure *ultimate_pixelPointer, pixel_structure *penultimate_pixelPointer){
+    pixel_structure cache[64];
+    int nbrIdenticalConsecutivePixels;
+    
+    
+}
