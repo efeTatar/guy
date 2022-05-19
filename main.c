f65af1a3ec3f4c1bac436460bfb607f65c326a4f
NathanSignoud <signoudnat@cy-tech.fr>
2022-05-19 15:54:04 +0200

    pixel pointer

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main(){
 
-    pixel_structure penultimate_pixel, ultimate_pixel, *pixelPointer;  
+    pixel_structure penultimate_pixel, ultimate_pixel, 
+        *penultimate_pixelPointer, *ultimate_pixelPointer;  
 
     //sets the initial penultimate_pixel values to 0 so that the first pixel be compared to rgb(0,0,0)
     penultimate_pixel.r = 0;
@@ -11,11 +12,8 @@ int main(){
 
     PPM_IMG* img = NULL;
     img = ppmOpen("calvinhobbes.ppm");
-    
     //FILE *fichier = NULL;
-    
-    
-	int x = 1, y = 1;
+    int x = 1, y = 1;
 	int total = 0;
 	int dec;
     printf("%d\n", ppmRead(img, x, y));
@@ -23,8 +21,8 @@ int main(){
     char Hex[6] = " ";
     DecimalToHex(Hex, dec);
     
-    pixelPointer = &penultimate_pixel;
-    HexToRGB(Hex, pixelPointer);
+    penultimate_pixelPointer = &penultimate_pixel;
+    HexToRGB(Hex, penultimate_pixelPointer);
     printf("RGB(%d, %d, %d)\n", penultimate_pixel.r, penultimate_pixel.g, penultimate_pixel.b);
     printf("Hello Efe\n");
 	
