f2132e71c019a56ba3470144b68ec2513f9492f7
NathanSignoud <signoudnat@cy-tech.fr>
2022-05-30 10:07:29 +0200

    Auto stash before merge of "main" and "origin/main"

--- main.c
+++ main.c
@@ -2,35 +2,48 @@
 
 int main(){
     PPM_IMG* img = NULL;
-    img = ppmOpen("testdsogv");
+    char nom[100], *Init = NULL;
+    int i, taille;
+    scanf("%s", nom);
+    taille = strlen(nom);
+    Init = malloc((taille+9)*sizeof(1));
+    Init[0] = 'i';Init[1] = 'm'; Init[2] = 'a'; Init[3] = 'g'; Init[4] = 'e'; Init[5] = 's'; Init[6] = '/';
+    for(i=0;i<taille+7;i++){
+        Init[i+7] = nom[i];
+    }
+    printf("%s", Init);
+    printf("\n");
+    img = ppmOpen(Init);
     FILE *fichier = NULL;
 	fichier = fopen("blabla", "wb+");
-    compressionManager(fichier, img);
+    //compressionManager(fichier, img);
     fclose(fichier);
     FILE *helo = NULL;
     ppmClose(img);
     printf("\nCOMPILED\n");
 
-    /*
+    
 	helo = fopen("blabla", "rb");
-    int i;
     unsigned char b = 0;
     int a;
     for(i=0;i<100;i++){
-        fread(&b, sizeof(unsigned char), 1, helo);
-        a = (int)b;
-        printf("\n%u", a);
+        fread(&b, sizeof(1), 1, helo);
+        printf("\n%u", b);
     }
-    fclose(helo);*/
+    fclose(helo);
     
     printf("\n");
     fichier = fopen("blabla", "rb");
     rewind(fichier);
-    decompressionManager(fichier);
+    //decompressionManager(fichier);
     fclose(fichier);
     ppmSave(img, "fml2");
     printf("SAVED");
+    free(Init);
     return 0;
 }
+
+
+
 
 
