ded055b9fb9d0e9f53ae71e4e0d87c8346ddc4e6
NathanSignoud <signoudnat@cy-tech.fr>
2022-05-31 09:31:04 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int Menu(){
+    int choix;
+    printf("        || DE(COM)PRESSION ||\nQue voulez vous faire :\n   -COMPRESSION (tapez 1).\n   -DECOMPRESSION (tapez 2);");
+    scanf("%d",&choix);
+    while(choix != & || choix != 0){
+        printf("Reeur veuillez choisir un nombre entre 0 et 1");
+    }
+    switch(choix){
+        case 1:
+            printf("\nncompression\nQuelle image voulez vous compresser (veuillez a bien deposer une image au format PPM.\n");
+            return 1;
+        case 0:
+            printf("\nDecompression\nQuel fichier voulez-vous decompresser : ");
+            return 0;
+        default: 
+            printf("ERROR");
+            break;
+    }
+    return -1;
+}
--- main.c
+++ main.c
@@ -2,24 +2,36 @@
 
 int main(){
     PPM_IMG* img = NULL;
-    /*char nom[100], *Init = NULL;
-    int i, taille;
-    scanf("%s", nom);
-    taille = strlen(nom);
-    Init = malloc((taille+9)*sizeof(1));
-    Init[0] = 'i';Init[1] = 'm'; Init[2] = 'a'; Init[3] = 'g'; Init[4] = 'e'; Init[5] = 's'; Init[6] = '/';
-    for(i=0;i<taille+7;i++){
-        Init[i+7] = nom[i];
+    char nom[1000], autreNom[1000];
+    int i, taille, choix;
+    
+    choix = Menu();
+    switch(choix){
+        case 1: 
+            char *Init = NULL, *cible = NULL;
+            FILE *fichier = NULL;
+            scanf("%s", nom);
+            taille = strlen(nom);
+            Init = malloc((taille+9)*sizeof(1));
+            Init[0] = 'i';Init[1] = 'm'; Init[2] = 'a'; Init[3] = 'g'; Init[4] = 'e'; Init[5] = 's'; Init[6] = '/';
+            for(i=0;i<taille+7;i++){
+                Init[i+7] = nom[i];
+            }
+            img = ppmOpen(Init);
+            printf("\nComment voulez vous appeller votre fichier compressé : ");
+            scanf("%s",autreNom);
+            taille = strlen(autreNom);
+            Init = malloc(taille*sizeof(1));
+            for(i=0;i<taille;i++){
+                cible[i] = autreNom[i];
+            }
+            fichier = fopen("autreNom", "wb+");
+            compressionManager(fichier, img);
+            fclose(fichier);
+            ppmClose(img);
+            printf("\nCOMPILED\n");
     }
-    printf("%s", Init);
-    printf("\n");*/
-    img = ppmOpen("testdsogv");
-    FILE *fichier = NULL;
-	fichier = fopen("blabla", "wb+");
-    compressionManager(fichier, img);
-    fclose(fichier);
-    ppmClose(img);
-    printf("\nCOMPILED\n");
+    FILE *helo = NULL;
 
     /*FILE *helo = NULL;
 	helo = fopen("blabla", "rb");
