3845dc2b9d9e0fcaa0dce8190e2f410e0f9b780a
Efe Tatar <tatarefe@cy-tech.fr>
2022-06-01 22:22:51 +0200

    22 22

--- src/debugTool.c
+++ src/debugTool.c
@@ -1,23 +1,35 @@
 #include "ppm_lib.h"
+#include <unistd.h>
 
 void debugtool(){
     PPM_IMG* img = NULL;
-    int x, y, h= 50, w = 50, r, g, b, chance;
+    int x, y, h= 1000, w = 1000, r, g, b, chance;
     img = ppmNew(w,h, 255, 3);
     ppmDisplay(img);
+    int a = 0;
     for(y=0;y<h;y++){
         for(x=0;x<w;x++){
-            chance = rand()%5;
+            /*chance = rand()%100;
             if(chance >= 80){
                 r = (rand()%256);
                 g = (rand()%256);
                 b = (rand()%256);
             }
+            ppmWrite(img, x, y, pixel(r, g, b));*/
+            if(a==0){
+                r = (rand()%256);
+                usleep(10); 
+                g = (rand()%256);
+                usleep(10); 
+                b = (rand()%256);
+                a=100;
+            }
             ppmWrite(img, x, y, pixel(r, g, b));
+            a--;
         }
     }
     ppmDisplay(img);
-    ppmSave(img,"testdsogv");
+    ppmSave(img,"testimage");
     ppmClose(img);
 }
 
