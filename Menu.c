c2a5fbd182ca05aafbc4f6e3469485fd9afd6a00
NathanSignoud <signoudnat@cy-tech.fr>
2022-06-01 10:30:02 +0200

    Auto stash before merge of "main" and "origin/main"

--- Menu.c
+++ Menu.c
@@ -4,15 +4,15 @@ int Menu(){
     int choix;
     printf("        || DE(COM)PRESSION ||\nQue voulez vous faire :\n   -COMPRESSION (tapez 1).\n   -DECOMPRESSION (tapez 2);");
     scanf("%d",&choix);
-    while(choix != 1 && choix != 0){
-        printf("Reeur veuillez choisir un nombre entre 0 et 1");
+    while(choix != 1 && choix != 2){
+        printf("Reeur veuillez choisir un nombre entre 0 et 2");
         scanf("%d",&choix);
     }
     switch(choix){
         case 1:
             printf("\nncompression\nQuelle image voulez vous compresser (veuillez a bien deposer une image au format PPM.\n");
             return 1;
-        case 0:
+        case 2:
             printf("\nDecompression\nQuel fichier voulez-vous decompresser : ");
             return 0;
         default: 
